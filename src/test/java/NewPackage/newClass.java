36f12b7fef11c14eeea8aeba8b2daa4e76198bf3
OmerOzdil <omerozdil54@gmail.com>
2021-11-04 17:37:01 +0000

    second commit

--- src/test/java/NewPackage/newClass.java
+++ src/test/java/NewPackage/newClass.java
@@ -4,5 +4,6 @@ public class newClass {
     public static void main(String[] args) {
         System.out.println("here is the first commit");
         System.out.println("This is coming from the feature branch");
+        System.out.println("This is coming from the feature branch fro second commit");
     }
 }
