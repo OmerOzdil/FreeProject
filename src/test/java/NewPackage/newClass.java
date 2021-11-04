2a09bee5616abaee64228b6da6093416a6eaa864
OmerOzdil <omerozdil54@gmail.com>
2021-11-04 17:58:26 +0000

    from this branch

--- src/test/java/NewPackage/newClass.java
+++ src/test/java/NewPackage/newClass.java
@@ -3,5 +3,6 @@ package NewPackage;
 public class newClass {
     public static void main(String[] args) {
         System.out.println("here is the first commit");
+        System.out.println("This is added from the newFeatureBranch1");
     }
 }
